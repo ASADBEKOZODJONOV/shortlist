30c976abe6b3b21c2cda51f5b6bd922c1f6dbddd
asadbek ozodjonov <asadmafiya000@gmail.com>
2022-04-16 17:40:14 +0500

    js faylda yandi funksiyalar

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let b = document.getElementById('btn').onclick = chexbox;
+let a = document.getElementById('ins');
+
+function chexbox() {
+    let node12 = document.createElement("button");
+    let textnode12 = document.createTextNode("Delete");
+    node12.appendChild(textnode12);
+    document.getElementById("myList").appendChild(node12);
+    node12.style.position = 'relative'
+    node12.style.right = '-30%'
+    node12.style.top = '20px'
+    let noed = document.createElement("p");
+    let textn = document.createTextNode(ins.value);
+    noed.appendChild(textn);
+    document.getElementById("myList").appendChild(noed);
+    node12.onclick = deletes;
+    node.style.position = "relative"
+
+    function deletes() {
+        node12.style.display = 'none'
+        noed.style.display = 'none'
+    }
+};
