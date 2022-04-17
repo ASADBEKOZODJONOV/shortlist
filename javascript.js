010102002f7abb54d9e7a6c0416c39d2a9e608c1
asadbek ozodjonov <asadmafiya000@gmail.com>
2022-04-17 13:57:26 +0500

    first commit

--- javascript.js
+++ javascript.js
@@ -2,22 +2,31 @@ let b = document.getElementById('btn').onclick = chexbox;
 let a = document.getElementById('ins');
 
 function chexbox() {
-    let node12 = document.createElement("button");
-    let textnode12 = document.createTextNode("Delete");
-    node12.appendChild(textnode12);
-    document.getElementById("myList").appendChild(node12);
-    node12.style.position = 'relative'
-    node12.style.right = '-30%'
-    node12.style.top = '20px'
-    let noed = document.createElement("p");
-    let textn = document.createTextNode(ins.value);
-    noed.appendChild(textn);
-    document.getElementById("myList").appendChild(noed);
-    node12.onclick = deletes;
-    node.style.position = "relative"
+    if (a.value != "") {
+        let node12 = document.createElement("button");
+        let textnode12 = document.createTextNode("Delete");
+        node12.appendChild(textnode12);
 
-    function deletes() {
-        node12.style.display = 'none'
-        noed.style.display = 'none'
+        document.getElementById("myList").appendChild(node12);
+
+        node12.style.position = 'relative'
+        node12.style.right = '-30%'
+        node12.style.top = '20px'
+        let noed = document.createElement("p");
+        let textn = document.createTextNode(ins.value);
+        a.value = ""
+        noed.appendChild(textn);
+        document.getElementById("myList").appendChild(noed);
+        node12.onclick = deletes;
+        node.style.position = "relative"
+
+        function deletes() {
+            node12.style.display = 'none'
+            noed.style.display = 'none'
+        }
+
+    } else {
+        alert("siz hech narsa kiritmadiz. iltimos kiriting!!!")
     }
+
 };
